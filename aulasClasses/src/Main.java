0eb9e500eeccd46b39c969ccfbfadac9919939a5
DANIELI BORDULIS PAIVA SILVA <danieli_b_silva@estudante.sesisenai.org.br>
2024-04-18 16:37:24 -0300

    classes

---
+++ aulasClasses/src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+ public class Main {
+
+
+ public static void main(String[] args){
+
+  Pessoa p= new Pessoa("Dani", 27);
+
+ //p.setidade(16);
+ // p.idade= p.idade + 1;
+ // p.setidade(p.getIdade() + 1);
+
+  System.out.println(p.getNome()+ "tem" + p.getIdade() + "anos");
+
+
+   }
+ }
